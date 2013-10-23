c9dac027e30c9dcf360400d3b902a99b73b8d464
Brian Coon <briancoon@yahoo.com>
2013-10-23 15:50:20 -0500

    added templates, static

--- twd/settings.py
+++ twd/settings.py
@@ -1,5 +1,11 @@
 # Django settings for twd project.
 
+import os
+PROJECT_PATH = os.getcwd()
+TEMPLATE_PATH = os.path.join(PROJECT_PATH, 'templates')
+STATIC_PATH = os.path.join(PROJECT_PATH, 'static')
+
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -72,6 +78,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    STATIC_PATH,
 )
 
 # List of finder classes that know how to find static files in
@@ -111,6 +118,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    TEMPLATE_PATH
 )
 
 INSTALLED_APPS = (
